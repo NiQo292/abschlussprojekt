b31b1000db11771f71f902aef7f26839b537d684
NiQo <n.haubold29@gmail.com>
2024-11-10 20:31:08 +0100

    finished ServiveOverview Component

--- components/Services/ServicesOverview.tsx
+++ components/Services/ServicesOverview.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { siteConfig } from "@/config/site";
 import Image from "next/image";
-import { Accordion, AccordionItem } from "@nextui-org/accordion";
 
 const ServicesOverview = () => {
   return (
     <section>
-      <div className="flex items-center border border-secondary40 px-5 py-3 text-dark2 dark:text-secondary40">
-        <Accordion>
+      <div className="bg-light dark:bg-secondary60">
+        <h1 className="px-5 pb-8 pt-16 text-32 text-red dark:text-secondary40 lg:w-[60%] lg:px-24 lg:text-72">
+          Wir packen mit an: <br />
+          unsere <span className="font-bold">Services.</span>
+        </h1>
+        <div className="grid grid-cols-1 lg:grid-cols-3">
           {siteConfig.servicesOverview.map((item, index) => (
-            <AccordionItem key={index} title={item.title}>
-              {item.description}
-            </AccordionItem>
+            <div
+              key={index}
+              className="flex items-start px-5 lg:flex-col lg:px-24"
+            >
+              <Image src={item.src} alt={item.title} />
+              <div className="pb-8">
+                <p className="font-bold mb-2">{item.title}</p>
+                {item.description}
+              </div>
+            </div>
           ))}
-        </Accordion>
+        </div>
       </div>
     </section>
   );
