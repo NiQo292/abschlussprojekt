508243ffc2f4d40273bb3902553045571030bf95
NiQo <n.haubold29@gmail.com>
2024-11-11 15:57:08 +0100

    Adjusted Map to be fully responsive and switch image on dark-/light-mode

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -6,8 +6,8 @@ import MapDark from "@/public/Map/mapdark.png";
 
 const Map = () => {
   return (
-    <section className="relative items-center justify-between pt-16 lg:flex lg:flex-row lg:pb-40 lg:pt-40 text-darkred dark:text-secondary h-[1000px] lg:h-auto">
-      <div className="absolute top-1/2 ms-5 -translate-y-1/2 transfrom lg:ms-24 lg:w-[50%] w-[60%]">
+    <section className="relative items-center justify-between pt-16 lg:flex lg:flex-row lg:pb-40 lg:pt-40 text-darkred dark:text-secondary h-[700px] lg:h-auto lg:min-h-[1000px]">
+      <div className="absolute top-1/2 ms-5 -translate-y-1/2 transfrom lg:ms-24 lg:w-[50%] w-[60%] z-50">
         <p className="mb-4 text-40 md:text-64 lg:text-72 xl:text-96 select-none">
           Wir freuen uns auf Ihren{" "}
           <span className="font-bold select-none">Besuch</span>
@@ -19,11 +19,20 @@ const Map = () => {
         </p>
         <button className="btn__outlined">Route berechnen</button>
       </div>
-      <div className="w-full h-full">
+      <div className="w-full h-full lg:min-h-[1000px] relative">
         <Image
           alt="Karte"
-          className="w-full h-full object-cover"
+          className="dark:hidden"
           src={MapLight}
+          layout="fill"
+          objectFit="cover"
+        />
+        <Image
+          alt="Karte"
+          className="hidden dark:block"
+          src={MapDark}
+          layout="fill"
+          objectFit="cover"
         />
       </div>
     </section>
