5e19e4bfe9305c2bd628c96f75b82d30cde1d1c5
NiQo <n.haubold29@gmail.com>
2024-11-11 11:54:08 +0100

    added elements

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,18 @@ import React from "react";
 import Carousel from "@/components/Carousel/Carousel";
 import Services from "@/components/Services/Services";
 import Products from "@/components/Products/Products";
+import Inspiration from "@/components/Inspiration/Inspiration";
+import Contact from "@/components/Contact/Contact";
+import Map from "@/components/Map/Map";
 export default function Home() {
   return (
     <div>
       <Carousel />
       <Services />
       <Products />
+      <Inspiration />
+      <Contact />
+      <Map />
     </div>
   );
 }
